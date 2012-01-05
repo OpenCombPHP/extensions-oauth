21257d8e9e8a24719c123b2cdef0979bd8693afe
alee <zq@alee2002.com>
2012-01-05 14:42:06 +0800

    create extension

---
+++ class/WebOpenAPI.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\webopenapi ;
+
+use org\opencomb\platform\ext\Extension ;
+
+class WebOpenAPI extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}
