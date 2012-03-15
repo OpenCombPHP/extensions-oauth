66ecc67dd32925517eb2e89674d5ed0861053644
gaojun <gaojun@zhenzhi.com>
2012-03-15 09:49:25 +0800

    1

--- packages/org.opencomb.oauth/OAuth.php
+++ packages/org.opencomb.oauth/OAuth.php
@@ -23,15 +23,5 @@ class OAuth extends Extension
 		PlatformSerializer::singleton()->addSystemObject(AdapterManager::singleton()) ;
 		
 		AOP::singleton()->register('org\\opencomb\\oauth\\aspect\\MainMenuAspect') ;
-		
-		//发布消息同步到weibo
-		AOP::singleton()->register('org\\opencomb\\oauth\\aspect\\UserStatePushStateAspect') ;
-		$aWeaveMgr->registerTemplate( 'userstate:CreateState.html', "/form@0/div@0/div@0/div@0/div@1/div@0", 'oauth:api/pushState.html', Patch::appendAfter ) ;
-		
-		//获取最新记录数然时候先拉取
-		AOP::singleton()->register('org\\opencomb\\oauth\\aspect\\UserStatePullStateAspect') ;
-		
-		//转发
-		$aWeaveMgr->registerTemplate( 'userstate:UserState.html', "/div@0/model:foreach@0/dl@0/dd@0/div@3/textarea@0", 'oauth:api/ForwardState.html', Patch::appendAfter ) ;
 	}
 }
