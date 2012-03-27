66f545bf329c234f8f3ea360c7f72eedbe255b23
kongyuan <anubiskong@gmail.com>
2012-03-27 13:43:00 +0800

    绑定帐号设置页面改进用户体验

--- packages/org.opencomb.oauth/controlPanel/OAuthState.php
+++ packages/org.opencomb.oauth/controlPanel/OAuthState.php
@@ -2,15 +2,12 @@
 namespace org\opencomb\oauth\controlPanel ;
 
 use org\jecat\framework\auth\IdManager;
-
 use org\jecat\framework\verifier\Length;
-
 use org\opencomb\platform\ext\Extension;
 use org\opencomb\oauth\adapter\AdapterManager;
 use org\opencomb\coresystem\mvc\controller\ControlPanel;
 use org\jecat\framework\message\Message;
 use org\opencomb\oauth\controlPanel\OAuthItem;
-
 
 class OAuthState extends ControlPanel
 {
@@ -22,7 +19,7 @@ class OAuthState extends ControlPanel
             'frame' => array(
                 	'class'=>'org\\opencomb\\coresystem\\mvc\\controller\\UserPanelFrame',
              ) ,
-			'view' => array(
+			'view:oauth' => array(
 				'template' => 'oauth:OAuthState.html' ,
 				'model' => 'auser' ,
 			) ,
@@ -44,6 +41,26 @@ class OAuthState extends ControlPanel
 	public function process()
 	{	
 	    $this->auser->load();
+	    
+	    //
+	    if(!$this->auser){
+	    	return;
+	    }
+	    $arrServices= array('t.qq.com', 'sohu.com' , '163.com' , 'weibo.com' ,'renren.com' ,'douban.com');
+	    $arrServiceModels = array();
+	    foreach($arrServices as $sService){
+	    	foreach($this->auser->childIterator() as $aModel){
+	    		if($aModel['service'] == $sService && $aModel['token'] !== ''){
+	    			$arrServiceModels[$sService] = $aModel;
+	    			break;
+	    		}
+	    	}
+	    	
+	    	if(!isset($arrServiceModels[$sService])){
+	    		$arrServiceModels[$sService] = null;
+	    	}
+	    }
+	    
+	    $this->viewOauth->variables()->set('arrServiceModels',$arrServiceModels) ;
 	}
-	
 }
