32decbeaf6bcfbb2adef203218883d07b33f46f1
alee <zq@alee2002.com>
2012-04-09 14:27:22 +0800

    使用menu buildBean 事件 替代 AOP

--- packages/org.opencomb.oauth/OAuth.php
+++ packages/org.opencomb.oauth/OAuth.php
@@ -7,6 +7,7 @@ use org\opencomb\platform\service\ServiceSerializer;
 use org\jecat\framework\ui\xhtml\weave\Patch;
 use org\jecat\framework\ui\xhtml\weave\WeaveManager;
 use org\opencomb\platform\ext\Extension ;
+use org\opencomb\platform\mvc\view\widget\Menu;
 
 class OAuth extends Extension 
 {
@@ -22,6 +23,24 @@ class OAuth extends Extension
 		// 提供给系统序列化
 		ServiceSerializer::singleton()->addSystemObject(AdapterManager::singleton()) ;
 		
-		AOP::singleton()->register('org\\opencomb\\oauth\\aspect\\MainMenuAspect') ;
+		
+
+		// 注册菜单build事件的处理函数
+		Menu::registerBuildHandle(
+				'org\\opencomb\\coresystem\\mvc\\controller\\ControlPanelFrame'
+				, 'frameView'
+				, 'mainMenu'
+				, array(__CLASS__,'buildControlPanelMenu')
+		) ;
+	}
+	
+	static public function buildControlPanelMenu(array & $arrConfig)
+	{
+		// 合并配置数组，增加菜单
+		$arrConfig['item:system']['item:platform-manage']['item:oauth']=array (
+		  		'title'=>'OAuth' ,
+		  		'link' => '?c=org.opencomb.oauth.controlPanel.OAuthSetting' ,
+		  		'query' => 'c=org.opencomb.oauth.controlPanel.OAuthSetting' ,
+		) ;
 	}
 }
